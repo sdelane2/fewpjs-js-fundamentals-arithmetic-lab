726ff2d26b05855ce71394b916ad1074bd5e3919
sdelane2 <seanmartindelaney@gmail.com>
2020-11-15 20:59:25 -0500

    math

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2 
+
+const random = Math.floor( Math.random() * 1000 ) + 1
+
+
+const mod = 16 % 6
+
+const max = Math.max( 5, 10, 15, 20)
